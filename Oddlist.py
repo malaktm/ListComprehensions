89085ec7a5795d156d43f1bd043c6efac8b5eb4d
malaktm <94327030+malaktm@users.noreply.github.com>
2022-09-20 23:57:54 +0300

    Update Oddlist.py

--- Oddlist.py
+++ Oddlist.py
@@ -1,3 +1,7 @@
+The odd_numbers function returns a list of odd numbers between 1 and n, inclusively. Fill in the blanks in the function, using list comprehension. Hint: remember that list and range counters start at 0 and end at the limit minus 1.
+
+
+
 def odd_numbers(n):
 	return [x for x in range(1,n+1) if x % 2 != 0]
 
